bed21bea5140087bf644ab12ab2858c8b8f2f292
amer <inayath_191@hotmail.com>
2023-02-15 19:10:45 +0530

    etere

--- new.java
+++ new.java
@@ -2,5 +2,16 @@ public class Main {
   public static void main(String[] args) {
     int myNum = 15;
     System.out.println(myNum);
+
+public class Main {
+  public static void main(String[] args) {
+    final int myNum = 15;
+    myNum = 20; // will generate an error
+    System.out.println(myNum);
+  }
+}
+
+
+
   }
 }
