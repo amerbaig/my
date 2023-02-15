c008d79dcbb61102e309b8be2d723958846c883a
amer <inayath_191@hotmail.com>
2023-02-15 19:08:52 +0530

    terer

--- new.java
+++ new.java
@@ -1,9 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
-	
-	
-	fereoitneorh
+    int myNum = 15;
+    System.out.println(myNum);
   }
-
 }
