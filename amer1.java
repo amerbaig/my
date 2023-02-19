b5499249c2cdfc846f9e6e8710eb1b50757f4f39
amer <inayath_191@hotmail.com>
2023-02-19 17:26:18 +0530

    asdf

--- amer1.java
+++ amer1.java
@@ -1,8 +1,9 @@
 public class Main {
-   public static void main(String[] args) {
-      String firstName = "John" ;
-      String lastName = "Doe" ;
-      String fullName = firstName + lastName;
-      System.out.println(fullName);
-   }
-}   
+  public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);  
+  }
+}
+
