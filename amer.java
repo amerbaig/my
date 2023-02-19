cce0ed6c5e74098b819d31a84accce8418916035
amer <inayath_191@hotmail.com>
2023-02-19 16:30:29 +0530

    ame

--- amer.java
+++ amer.java
@@ -1,7 +1,11 @@
-Public class Main {
-   public static void main (string[] args) {
-		System.out.println("Hello World!");
-		System.out.println("I am learning java.");
-		System.out.println("It is awesome!");
-   }
+
+public Class Amer{
+	int x = 5;
+	public static void main(String[] args){
+		
+		Amer am = new Amer();
+		
+		System.out.println(am.x);
+		
+	}
 }
